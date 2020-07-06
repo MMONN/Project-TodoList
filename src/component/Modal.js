76ccd439b7d5fa203e58f40ef799b22ad46f4f50
MMONN <chatchanok.cm@mail.kmutt.ac.th>
2020-07-06 13:53:23 +0700

    [Create] modal to add item

---
+++ src/component/Modal.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import Modal from 'react-bootstrap/Modal';
+import ModalDialog from 'react-bootstrap/ModalDialog';
+import ModalHeader from 'react-bootstrap/ModalHeader';
+import ModalTitle from 'react-bootstrap/ModalTitle';
+import ModalBody from 'react-bootstrap/ModalBody';
+import ModalFooter from 'react-bootstrap/ModalFooter';
+import Button from 'react-bootstrap/Button';
+
+
+function Modals(props) {
+      return (
+            <>
+                  <Modal.Dialog>
+                        <Modal.Header closeButton>
+                              <Modal.Title>Modal title</Modal.Title>
+                        </Modal.Header>
+
+                        <Modal.Body>
+                              <p>Modal body text goes here.</p>
+                        </Modal.Body>
+
+                        <Modal.Footer>
+                              <Button variant="secondary">Close</Button>
+                              <Button variant="primary">Save changes</Button>
+                        </Modal.Footer>
+                  </Modal.Dialog>
+            </>
+      )
+}
+export default Modals;
